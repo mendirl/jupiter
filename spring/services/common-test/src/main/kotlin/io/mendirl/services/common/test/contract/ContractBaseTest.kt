3283f3c283e73a85945a72738a6f216c41dc5daf
Fabien Couillard <couillard.fabien+dev@gmail.com>
2022-03-20 11:12:28 +0100

    more ignore

--- spring/services/common-test/src/main/kotlin/io/mendirl/services/common/test/contract/ContractBaseTest.kt
+++ spring/services/common-test/src/main/kotlin/io/mendirl/services/common/test/contract/ContractBaseTest.kt
@@ -1,12 +1,8 @@
 package io.mendirl.services.common.test.contract
 
-import dasniko.testcontainers.keycloak.KeycloakContainer
 import org.junit.jupiter.api.Disabled
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.DynamicPropertyRegistry
-import org.springframework.test.context.DynamicPropertySource
-import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 
 @Disabled
@@ -15,17 +11,17 @@ import org.testcontainers.junit.jupiter.Testcontainers
 @ActiveProfiles(value = ["test"])
 class ContractBaseTest {
 
-    companion object {
-        @Container
-        private val keycloak =
-            KeycloakContainer().withRealmImportFile("realm-jupiter.json")
-
-        @DynamicPropertySource
-        @JvmStatic
-        fun registerPgProperties(registry: DynamicPropertyRegistry) {
-            registry.add("jupiter.security.oauth2.url") {
-                "http://localhost:${keycloak.httpPort}/auth/realms/JupiterR"
-            }
-        }
-    }
+//    companion object {
+//        @Container
+//        private val keycloak =
+//            KeycloakContainer().withRealmImportFile("realm-jupiter.json")
+//
+//        @DynamicPropertySource
+//        @JvmStatic
+//        fun registerPgProperties(registry: DynamicPropertyRegistry) {
+//            registry.add("jupiter.security.oauth2.url") {
+//                "http://localhost:${keycloak.httpPort}/auth/realms/JupiterR"
+//            }
+//        }
+//    }
 }
